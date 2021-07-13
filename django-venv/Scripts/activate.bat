69f1dea0af99a6b3249588630189db38c4290bc1
Junhong0209 <63082558+Junhong0209@users.noreply.github.com>
2021-07-13 10:10:41 +0900

    .

--- django-venv/Scripts/activate.bat
+++ django-venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\2021_DGSW\Coding\Hackerton\django-venv
+set VIRTUAL_ENV=C:\2021_DGSW\Coding\Server\django-venv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- smarthome/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('main/', views.getAllSensor),
-    path('main/gasPost/', views.gasPost),
-]
